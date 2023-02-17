0235c0f68cf5db920a0ad65536af2c1003312a34
DiegoSilva89 <diegosantosdasilva89@gmail.com>
2023-02-17 04:59:14 -0300

    corrigindo conflito

--- Módulo 4 - Configurando IDE/M4PrimeiroProjetoIntelliJ/src/Main.java
+++ Módulo 4 - Configurando IDE/M4PrimeiroProjetoIntelliJ/src/Main.java
@@ -1,10 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
 
-        System.out.println("Hello world!");
-=======
         System.out.println("Hello Diego!");
->>>>>>> a8de6d4c4803a3a7d3864fd0f2d2447f1a09ee31
+        
     }
 }
