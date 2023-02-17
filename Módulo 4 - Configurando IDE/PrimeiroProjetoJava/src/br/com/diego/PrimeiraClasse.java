2e1d4e742067a5fcf69d1f6a5aae7cd2dbcb8e0c
DiegoSilva89 <diegosantosdasilva89@gmail.com>
2023-02-17 02:31:28 -0300

    aula de IntelliJ e Eclipse

---
+++ Módulo 4 - Configurando IDE/PrimeiroProjetoJava/src/br/com/diego/PrimeiraClasse.java
@@ -0,0 +1,16 @@
+package br.com.diego;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) {
+		
+		String ambiente = args[0];
+		
+		if (ambiente.equals("DEV")) {
+			System.out.println(ambiente);			
+		}		
+		
+
+	}
+
+}
